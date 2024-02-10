806288ea37b5117759c98876a0d61119c5c8f171
jeongmoon <jm5836@naver.com>
2024-02-10 19:13:41 +0900

    ui 수장, 카카오 보내기 추가

--- assets/js/addonByJm.js
+++ assets/js/addonByJm.js
@@ -1,9 +1,12 @@
-/**
+/*********************************
  * 기존 템플릿에 추가하는 소스들
- */
+ * 
+ *********************************/
 
-// audio가 load 될 때 자동재생 됨
-var audio = new Audio('audio/song1.mp3');
+const accountCopyBtn = document.querySelectorAll('.account-copy-btn');
+const shareLinkCopy = document.getElementById('share-link-copy');
+
+const audio = new Audio('audio/song1.mp3');		//audio가 load 될 때 자동재생 됨
 audio.autoplay = true;
 
 $(document).ready(function(){
@@ -13,25 +16,23 @@ $(document).ready(function(){
 	
 	//alert('hi');
 	audio.play();
+	setBgmIcon();
 	
+	//디데이 셋팅
 	setDDay();
 	
+	//갤러리 초기화
 	setPhotoWwipe();
-	
-	
-	makeModal();
 	
 	
 	
 });
 
-function playBgMusic(){
-	 if( audio.paused )
-		 audio.play();
-	  else
-		  audio.pause();
-}
 
+/**
+ * 디데이 셋팅
+ * @returns
+ */
 function setDDay(){
 	const today = new Date();
 	const dday = new Date("Jun 29,2024, 00:00:00");
@@ -44,6 +45,10 @@ function setDDay(){
 	
 }
 
+/**
+ * 사진 초기화
+ * @returns
+ */
 function setPhotoWwipe(){
 	$('.gallery-one').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
@@ -55,8 +60,7 @@ function setPhotoWwipe(){
     -----------------------------------------*/
     $('#tmNavbar a').click(function(){
         $('#tmNavbar').collapse('hide');
-
-       // adjustHeightOfPage($(this).data("no")); // Adjust page height       
+  
     });
 
     /* Browser resized 
@@ -64,43 +68,131 @@ function setPhotoWwipe(){
     $( window ).resize(function() {
         var currentPageNo = $(".cd-hero-slider li.selected .js-tm-page-content").data("page-no");
         
-        // wait 3 seconds
-        //setTimeout(function() {
-        //    adjustHeightOfPage( currentPageNo );
-        //}, 1000);
-        
     });
 
     // Remove preloader (https://ihatetomatoes.net/create-custom-preloading-screen/)
     $('body').addClass('loaded');
 }
 
-function makeModal() {
-	/*$("div[id^='myModal']").each(function(){
-		  
-		  var currentModal = $(this);
-		  
-		  //click next
-		  currentModal.find('.btn-next').click(function(){
-		    currentModal.modal('hide');
-		    currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
-		  });
-		  
-		  //click prev
-		  currentModal.find('.btn-prev').click(function(){
-		    currentModal.modal('hide');
-		    currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
-		  });
-
-		});*/
-}
-
+/**
+ * 음악 버튼
+ * @returns
+ */
 function actionToggle() {
-	  const action = document.querySelector('.action');
+	  
+	// 버튼 active 클래스 토글 (회전)
+	const action = document.querySelector('.action');
 	  action.classList.toggle('active');
 	  
+	 // 음악 재생,정지
 	  if( audio.paused )
 			 audio.play();
-		  else
-			  audio.pause();
-	}
+	  else
+		  audio.pause();
+	  
+	  // 아이콘 설정
+	  setBgmIcon();
+}
+
+/**
+ * 배경음악 재생, 정지
+ * @returns
+ */
+function setBgmIcon(){
+	 if( audio.paused ){
+		 $("#musicIcon").removeClass( 'fa-volume-up' );
+		 $("#musicIcon").addClass( 'fa-volume-mute' );
+	 }
+		 
+	  else{
+		 $("#musicIcon").removeClass( 'fa-volume-mute' );
+		 $("#musicIcon").addClass( 'fa-volume-up' );
+		  
+	  }
+}
+
+
+accountCopyBtn.forEach((e, index) => {
+	e.addEventListener('click', () => {
+    window.navigator.clipboard.writeText(
+      accountCopyBtn[index].parentElement.querySelector('.col-1').textContent
+       + ' ' +  accountCopyBtn[index].parentElement.querySelector('.col-2').textContent.replaceAll('-', '')
+    ).then(
+      () => {
+        alert(`계좌번호가 복사되었습니다.`);
+      },
+      () => {
+        try {
+          const tempAccountText = document.createElement("textarea");
+          tempAccountText.value = accountCopyBtn[index].parentElement.querySelector('.col-1').textContent
+              + ' ' + accountCopyBtn[index].parentElement.querySelector('.col-2').textContent.replaceAll('-', '');
+          tempAccountText.style.cssText = "position:absolute;left:-9999px;top:-9999px";
+          document.body.appendChild(tempAccountText);
+          tempAccountText.select();
+          document.execCommand("copy");
+          alert(`계좌번호가 복사되었습니다.`);
+        } catch(err) {
+          alert(`계좌 복사 실패 (${err})! 연락처로 문의 부탁드립니다!`);
+        }
+
+        document.body.removeChild(tempAccountText);
+      }
+    );
+  });
+});
+
+/*shareLinkCopy.addEventListener('click', () => {
+  window.navigator.clipboard.writeText(document.location.href).then(
+    () => {
+      alert('주소 복사 완료!');
+    },
+    () => {
+      try {
+        const tempShareLink = document.createElement("textarea");
+        tempShareLink.value = document.location.href;
+        tempShareLink.style.cssText = "position:absolute;left:-9999px;top:-9999px";
+        document.body.appendChild(tempShareLink);
+        tempShareLink.select();
+        document.execCommand("copy");
+        alert('주소 복사 완료!');
+      } catch(err) {
+        alert(`주소 복사 실패 (${err})! 아래 연락처로 문의 부탁드립니다!`);
+      }
+
+      document.body.removeChild(tempShareLink);
+    }
+  );
+});
+
+Kakao.init('199d271581eb40b8d632a008fdd8fe11');
+
+Kakao.Share.createCustomButton({
+  container: '#kakaotalk-sharing-btn',
+  templateId: 97955,
+});*/
+
+Kakao.init('199d271581eb40b8d632a008fdd8fe11');
+
+Kakao.Share.createDefaultButton({
+		container: '#kakaotalk-sharing-btn',
+		objectType: 'feed',
+		content: {
+		    title: '양경환 ♥ 이정문 결혼합니다.',
+		    description: '6월 29일 (토) 12시 30분 \n 더채플앳논현 5층 라메르홀',
+		    imageUrl:
+		      'https://jeongmoon417.github.io/wedding_card/images/banner2.jpg',
+		    link: {
+		      mobileWebUrl: 'https://jeongmoon417.github.io/wedding_card/',
+		      webUrl: 'https://jeongmoon417.github.io/wedding_card/',
+		    },
+	    },
+    buttons: [
+      {
+        title: '모바일 청첩장 보기',
+        link: {
+          mobileWebUrl: 'https://jeongmoon417.github.io/wedding_card/',
+          webUrl: 'https://jeongmoon417.github.io/wedding_card/',
+        },
+      }
+    ],
+});
